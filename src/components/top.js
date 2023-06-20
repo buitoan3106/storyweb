efd68ff547e27fb7ed778e616502a143ac2c8209
buitoan3106 <buiductoanfu@gmail.com>
2023-06-20 18:39:37 +0700

    first-frontend

--- src/components/top.js
+++ src/components/top.js
@@ -1,16 +1,23 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 export default function Top() {
     return (
-        <header>
+        <header className="header">
             <div className="container fluid Menu">
                 <div className="row">
-                    <div className="col-12 col-sm-8">
+                    <div className="col-lg-2">
+                        <div className="header__logo">
+                            <a href="./home">
+                                <img src={require("./image/logo.png")} alt="" />
+                            </a>
+                        </div>
+                    </div>
+                    <div className="col-lg-8 header__menu mobile-menu">
                         <ul className="left">
-                            <li><Link to='/'>Home</Link></li>
-                            <li><Link to='/about'>About Us</Link></li>
+                            <li><NavLink to={'/'} className={({ isActive }) => isActive ? 'link-active' : 'link'}>Home</NavLink></li>
+                            <li><NavLink to={'/about'} className={({ isActive }) => isActive ? 'link-active' : 'link'}>About Us</NavLink></li>
                         </ul>
                     </div>
-                    <div className="col-12 col-sm-4">
+                    <div className="col-lg-2">
                         <ul className="right">
                             <li>
                                 <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#signInModal">
--- src/layouts/defaultlayout.js
+++ src/layouts/defaultlayout.js
@@ -3,14 +3,17 @@ import Content from "../components/content";
 import Footer from "../components/footer";
 import React, { Fragment } from "react";
 import "../styles/defaultlayout.css";
+import { Container } from "react-bootstrap";
 
 export default function DefaultLayout({ className = "container fluid", children }) {
     return (
         <>
-            <Top />
-            <Content />
-            <div className={className}>{children}</div>
-            <Footer />
+            <Container fluid>
+                <Top />
+                <Content />
+                <div className={className}>{children}</div>
+                <Footer />
+            </Container>
         </>
     )
 }
