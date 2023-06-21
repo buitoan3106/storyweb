f434b3b34e5597c2af9b361251804013b903e0f2
buitoan3106 <buiductoanfu@gmail.com>
2023-06-21 23:43:15 +0700

    Front end home page and login page

--- src/screens/home.js
+++ src/screens/home.js
@@ -5,9 +5,276 @@ export default function Home() {
     return (
         <DefaultLayout>
             <Content></Content>
-            <div className="row">
-
-            </div>
+            <section className="product spad">
+                <div class="row">
+                    <div class="col-lg-8">
+                        <div class="trending__product">
+                            <div class="row">
+                                <div class="col-lg-8 col-md-8 col-sm-8">
+                                    <div class="section-title">
+                                        <h4>Trending Now</h4>
+                                    </div>
+                                </div>
+                                <div class="col-lg-4 col-md-4 col-sm-4">
+                                    <div class="btn__all">
+                                        <a href="#" class="primary-btn">View All <span class="arrow_right"></span></a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-lg-4 col-md-6 col-sm-6">
+                                    <div class="product__item">
+                                        <div class="product__item__pic set-bg" data-setbg="./10.jpg">
+                                            <div class="ep">18 / 18</div>
+                                            <div class="comment"><i class="fa fa-comments"></i> 11</div>
+                                            <div class="view"><i class="fa fa-eye"></i> 9141</div>
+                                        </div>
+                                        <div class="product__item__text">
+                                            <h5><a href="#">Kẻ Phản Bội</a></h5>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-4 col-md-6 col-sm-6">
+                                    <div class="product__item">
+                                        <div class="product__item__pic set-bg" data-setbg={require("./10.jpg")}>
+                                            <div class="ep">18 / 18</div>
+                                            <div class="comment"><i class="fa fa-comments"></i> 11</div>
+                                            <div class="view"><i class="fa fa-eye"></i> 9141</div>
+                                        </div>
+                                        <div class="product__item__text">
+                                            <h5><a href="#">Ta làm tham quan các nàng lại nói ta là trung thần</a></h5>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-4 col-md-6 col-sm-6">
+                                    <div class="product__item">
+                                        <div class="product__item__pic set-bg" data-setbg="img/trending/trend-3.jpg">
+                                            <div class="ep">18 / 18</div>
+                                            <div class="comment"><i class="fa fa-comments"></i> 11</div>
+                                            <div class="view"><i class="fa fa-eye"></i> 9141</div>
+                                        </div>
+                                        <div class="product__item__text">
+                                            <h5><a href="#">Mịch tiên kiếm đạo</a></h5>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-4 col-md-6 col-sm-6">
+                                    <div class="product__item">
+                                        <div class="product__item__pic set-bg" data-setbg="img/trending/trend-4.jpg">
+                                            <div class="ep">18 / 18</div>
+                                            <div class="comment"><i class="fa fa-comments"></i> 11</div>
+                                            <div class="view"><i class="fa fa-eye"></i> 9141</div>
+                                        </div>
+                                        <div class="product__item__text">
+                                            <h5><a href="#">Thiên hạ đệ nhất thần</a></h5>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-4 col-md-6 col-sm-6">
+                                    <div class="product__item">
+                                        <div class="product__item__pic set-bg" data-setbg="img/trending/trend-5.jpg">
+                                            <div class="ep">18 / 18</div>
+                                            <div class="comment"><i class="fa fa-comments"></i> 11</div>
+                                            <div class="view"><i class="fa fa-eye"></i> 9141</div>
+                                        </div>
+                                        <div class="product__item__text">
+                                            <h5><a href="#">Giang hồ khắp nơi là áo choàng</a></h5>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-4 col-md-6 col-sm-6">
+                                    <div class="product__item">
+                                        <div class="product__item__pic set-bg" data-setbg="img/trending/trend-6.jpg">
+                                            <div class="ep">18 / 18</div>
+                                            <div class="comment"><i class="fa fa-comments"></i> 11</div>
+                                            <div class="view"><i class="fa fa-eye"></i> 9141</div>
+                                        </div>
+                                        <div class="product__item__text">
+                                            <h5><a href="#">Kiếm vực vô địch</a></h5>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="popular__product">
+                            <div class="row">
+                                <div class="col-lg-8 col-md-8 col-sm-8">
+                                    <div class="section-title">
+                                        <h4>Popular Shows</h4>
+                                    </div>
+                                </div>
+                                <div class="col-lg-4 col-md-4 col-sm-4">
+                                    <div class="btn__all">
+                                        <a href="#" class="primary-btn">View All <span class="arrow_right"></span></a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-lg-4 col-md-6 col-sm-6">
+                                    <div class="product__item">
+                                        <div class="product__item__pic set-bg" data-setbg="img/popular/popular-1.jpg">
+                                            <div class="ep">18 / 18</div>
+                                            <div class="comment"><i class="fa fa-comments"></i> 11</div>
+                                            <div class="view"><i class="fa fa-eye"></i> 9141</div>
+                                        </div>
+                                        <div class="product__item__text">
+                                            <h5><a href="#">Vạn cổ ma tôn</a></h5>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-4 col-md-6 col-sm-6">
+                                    <div class="product__item">
+                                        <div class="product__item__pic set-bg" data-setbg="img/popular/popular-2.jpg">
+                                            <div class="ep">18 / 18</div>
+                                            <div class="comment"><i class="fa fa-comments"></i> 11</div>
+                                            <div class="view"><i class="fa fa-eye"></i> 9141</div>
+                                        </div>
+                                        <div class="product__item__text">
+                                            <h5><a href="#">Đệ nhất tần tranh</a></h5>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-4 col-md-6 col-sm-6">
+                                    <div class="product__item">
+                                        <div class="product__item__pic set-bg" data-setbg="img/popular/popular-3.jpg">
+                                            <div class="ep">18 / 18</div>
+                                            <div class="comment"><i class="fa fa-comments"></i> 11</div>
+                                            <div class="view"><i class="fa fa-eye"></i> 9141</div>
+                                        </div>
+                                        <div class="product__item__text">
+                                            <h5><a href="#">Hậu duệ kiếm thần</a></h5>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-4 col-md-6 col-sm-6">
+                                    <div class="product__item">
+                                        <div class="product__item__pic set-bg" data-setbg="img/popular/popular-4.jpg">
+                                            <div class="ep">18 / 18</div>
+                                            <div class="comment"><i class="fa fa-comments"></i> 11</div>
+                                            <div class="view"><i class="fa fa-eye"></i> 9141</div>
+                                        </div>
+                                        <div class="product__item__text">
+                                            <h5><a href="#">Thần điêu hiệp nữ</a></h5>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-4 col-md-6 col-sm-6">
+                                    <div class="product__item">
+                                        <div class="product__item__pic set-bg" data-setbg="img/popular/popular-5.jpg">
+                                            <div class="ep">18 / 18</div>
+                                            <div class="comment"><i class="fa fa-comments"></i> 11</div>
+                                            <div class="view"><i class="fa fa-eye"></i> 9141</div>
+                                        </div>
+                                        <div class="product__item__text">
+                                            <h5><a href="#">Em nói anh nghe chuyện yêu đương</a></h5>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-4 col-md-6 col-sm-6">
+                                    <div class="product__item">
+                                        <div class="product__item__pic set-bg" data-setbg="img/popular/popular-6.jpg">
+                                            <div class="ep">18 / 18</div>
+                                            <div class="comment"><i class="fa fa-comments"></i> 11</div>
+                                            <div class="view"><i class="fa fa-eye"></i> 9141</div>
+                                        </div>
+                                        <div class="product__item__text">
+                                            <h5><a href="#">Thì ra cậu ấy thích tôi</a></h5>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="recent__product">
+                            <div class="row">
+                                <div class="col-lg-8 col-md-8 col-sm-8">
+                                    <div class="section-title">
+                                        <h4>Recently Added</h4>
+                                    </div>
+                                </div>
+                                <div class="col-lg-4 col-md-4 col-sm-4">
+                                    <div class="btn__all">
+                                        <a href="#" class="primary-btn">View All <span class="arrow_right"></span></a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-lg-4 col-md-6 col-sm-6">
+                                    <div class="product__item">
+                                        <div class="product__item__pic set-bg" data-setbg="img/recent/recent-1.jpg">
+                                            <div class="ep">18 / 18</div>
+                                            <div class="comment"><i class="fa fa-comments"></i> 11</div>
+                                            <div class="view"><i class="fa fa-eye"></i> 9141</div>
+                                        </div>
+                                        <div class="product__item__text">
+                                            <h5><a href="#">Võ thành ti</a></h5>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-4 col-md-6 col-sm-6">
+                                    <div class="product__item">
+                                        <div class="product__item__pic set-bg" data-setbg="img/recent/recent-2.jpg">
+                                            <div class="ep">18 / 18</div>
+                                            <div class="comment"><i class="fa fa-comments"></i> 11</div>
+                                            <div class="view"><i class="fa fa-eye"></i> 9141</div>
+                                        </div>
+                                        <div class="product__item__text">
+                                            <h5><a href="#">Ước hẹn một đời vì nhau</a></h5>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-4 col-md-6 col-sm-6">
+                                    <div class="product__item">
+                                        <div class="product__item__pic set-bg" data-setbg="img/recent/recent-3.jpg">
+                                            <div class="ep">18 / 18</div>
+                                            <div class="comment"><i class="fa fa-comments"></i> 11</div>
+                                            <div class="view"><i class="fa fa-eye"></i> 9141</div>
+                                        </div>
+                                        <div class="product__item__text">
+                                            <h5><a href="#">Hạnh phúc là khi được gặp em</a></h5>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-4 col-md-6 col-sm-6">
+                                    <div class="product__item">
+                                        <div class="product__item__pic set-bg" data-setbg="img/recent/recent-4.jpg">
+                                            <div class="ep">18 / 18</div>
+                                            <div class="comment"><i class="fa fa-comments"></i> 11</div>
+                                            <div class="view"><i class="fa fa-eye"></i> 9141</div>
+                                        </div>
+                                        <div class="product__item__text">
+                                            <h5><a href="#">Sau khi ra khỏi viện tâm thần</a></h5>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-4 col-md-6 col-sm-6">
+                                    <div class="product__item">
+                                        <div class="product__item__pic set-bg" data-setbg="img/recent/recent-5.jpg">
+                                            <div class="ep">18 / 18</div>
+                                            <div class="comment"><i class="fa fa-comments"></i> 11</div>
+                                            <div class="view"><i class="fa fa-eye"></i> 9141</div>
+                                        </div>
+                                        <div class="product__item__text">
+                                            <h5><a href="#">Ta nuôi một ma quân</a></h5>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-4 col-md-6 col-sm-6">
+                                    <div class="product__item">
+                                        <div class="product__item__pic set-bg" data-setbg="img/recent/recent-6.jpg">
+                                            <div class="ep">18 / 18</div>
+                                            <div class="comment"><i class="fa fa-comments"></i> 11</div>
+                                            <div class="view"><i class="fa fa-eye"></i> 9141</div>
+                                        </div>
+                                        <div class="product__item__text">
+                                            <h5><a href="#">Gương kia ngự ở trên tường</a></h5>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </DefaultLayout>
     )
 }
