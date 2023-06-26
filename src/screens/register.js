cddab666acc9b890ea52e0103c5c3ad59598cfe2
Le Trung Anh (K16_HL) <anhlthe163676@fpt.edu.vn>
2023-06-26 21:11:05 +0700

    register.js

--- src/screens/register.js
+++ src/screens/register.js
@@ -16,28 +16,44 @@ export default function Register() {
     return (
         <DefaultLayout>
             <form onSubmit={handleSubmit}>
-                <label>
-                    Username:<br></br>
-                    <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Enter your username"/>
-                </label><br></br>
-                <label>
-                    Password:<br></br>
-                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Enter your password"/>
-                </label><br></br>
-                <label>
-                    Name:<br></br>
-                    <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder="Enter your name"/>
-                </label><br></br>
-                <label>
-                    Age:<br></br>
-                    <input type="number" value={age} onChange={(e) => setAge(e.target.value)} placeholder="Enter your age"/>
-                </label><br></br>
-                <label>
-                    Email:<br></br>
-                    <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Enter your Email"/>
-                </label><br></br>
+                <div className="row">
+                    <div className="col-lg-6" style={{borderRight:'1px solid white'}}>
+                        <h3>Sign Up</h3>
+                        <br />
+                        <br />
+                        <label>
+                            Username:<br></br>
+                            <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Enter your username" />
+                        </label><br></br>
+                        <label>
+                            Password:<br></br>
+                            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Enter your password" />
+                        </label><br></br>
+                        <label>
+                            Name:<br></br>
+                            <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder="Enter your name" />
+                        </label><br></br>
+                        <label>
+                            Age:<br></br>
+                            <input type="number" value={age} onChange={(e) => setAge(e.target.value)} placeholder="Enter your age" />
+                        </label><br></br>
+                        <label>
+                            Email:<br></br>
+                            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Enter your Email" />
+                        </label><br></br>
+                        <button type="submit">Register Now</button>
+                        <br/>
+                        <br/>
+                        <p>Already have an account? <a href="/login" style={{color: 'red'}}>Log In!</a></p>
+                    </div>
+
+                    <div className="col-lg-6">
+                    <img src={require("../components/image/11.jpg")} style={{width:'80%'}}/>
+                    </div>
+
+                </div>
+
                 
-                <button type="submit">Register</button>
             </form>
         </DefaultLayout>
     );
