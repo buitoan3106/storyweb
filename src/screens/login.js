d3a9ec188e8cfa545ceb1238440e6b4f0fcbfce9
buitoan3106 <buiductoanfu@gmail.com>
2023-06-29 00:10:30 +0700

    change linh tinh

--- src/screens/login.js
+++ src/screens/login.js
@@ -7,21 +7,56 @@ export default function Login() {
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        console.log(`Username: ${username}, Password: ${password}`);
     };
     return (
         <DefaultLayout>
-            <form onSubmit={handleSubmit}>
-                <label>
-                    Username:<br></br>
-                    <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Enter your username" />
-                </label><br></br>
-                <label>
-                    Password:<br></br>
-                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Enter your password" />
-                </label><br></br>
 
-                <button type="submit">Login</button>
-            </form>
+            <div className="row login">
+                <div className="col-lg-6">
+                    <div className="row">
+                        <h3>Login</h3>
+                        <br />
+                        <br />
+                        <form onSubmit={handleSubmit}>
+                            <label>
+                                Username<br></br>
+                                <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Enter your username" />
+                            </label>
+                            <br />
+                            <br />
+                            <label>
+                                Password<br></br>
+                                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Enter your password" />
+                            </label>
+                            <br></br>
+
+
+
+                            <div className="row">
+                                <div className="col-lg-6" style={{ paddingLeft: '50px' }}>
+                                    <button type="submit">Login Now</button>
+                                </div>
+
+                                <div className="col-lg-6" style={{ marginTop: '25px' }}>
+                                    <a href="/">Forgot Your Password?</a>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <br />
+                    <br />
+
+                </div>
+
+                <div className="col-lg-6" style={{ borderLeft: '1px solid white' }}>
+                    <div style={{ marginLeft: '30px' }}>
+                        <h3>Don’t Have An Account?</h3>
+                        <button type="submit">Rigister Now</button>
+                    </div>
+                </div>
+            </div>
+
         </DefaultLayout>
     )
 }
