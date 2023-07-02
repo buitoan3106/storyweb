bad1bb6c3a29e22292c9fe56d4bee3b83ef111db
buitoan3106 <buiductoanfu@gmail.com>
2023-07-03 00:22:40 +0700

    category

--- src/screens/category.js
+++ src/screens/category.js
@@ -1,181 +1,180 @@
-import Content from "../components/content";
 import DefaultLayout from "../layouts/defaultlayout";
-import img1 from "../assets/images/1.jpg";
-
-import "./../styles/storydetails.css";
-import StoryItem from "./../components/storyitem";
-import StoryItem2 from "./../components/storyitem2";
-import StoryItem3 from "./../components/storyitem3";
+import { useState, useEffect } from "react";
 
 export default function Category() {
+  const [story, setStory] = useState([]);
+  useEffect(() => {
+    fetch('http://localhost:9999/story')
+      .then(response => response.json())
+      .then(json => setStory(json));
+  }, []);
+  const [category, setCategory] = useState([]);
+  useEffect(() => {
+    fetch('http://localhost:9999/category')
+      .then(response => response.json())
+      .then(json => setCategory(json))
+  }, [])
   return (
     <DefaultLayout>
-      <section classNameName="product-page spad">
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-8">
-              <div className="product__page__content">
-                <div className="product__page__title">
-                  <div className="row">
-                    <div className="col-lg-8 col-md-8 col-sm-6">
-                      <div className="section-title">
-                        <h4>Romance</h4>
+      <section class="product-page spad">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-8">
+              {
+                category.map(cate => {
+                  <div class="product__page__content">
+                    <div class="product__page__title">
+                      <div class="row">
+                        <div class="col-lg-8 col-md-8 col-sm-6">
+                          <div class="section-title">
+                            <h4>{cate.categoryName}</h4>
+                          </div>
+                        </div>
+                        <div class="col-lg-4 col-md-4 col-sm-6">
+                          <div class="product__page__filter">
+                            <p>Order by:</p>
+                            <select>
+                              <option value="">A-Z</option>
+                              <option value="">1-10</option>
+                              <option value="">10-50</option>
+                            </select>
+                          </div>
+                        </div>
                       </div>
                     </div>
-                    <div className="col-lg-4 col-md-4 col-sm-6">
-                      <div className="product__page__filter">
-                        <p>Order by:</p>
-                        <select>
-                          <option value="">A-Z</option>
-                          <option value="">1-10</option>
-                          <option value="">10-50</option>
-                        </select>
-                      </div>
+                    <div class="row">
+                      {
+                        story.map(s => {
+                          if (s.categoryName === cate.categoryName) {
+                            return (
+                              <div class="col-lg-4 col-md-6 col-sm-6">
+                                <div class="product__item">
+                                  <div class="product__item__pic" style={{ backgroundImage: `url(${require("../components/image/" + s.image)})`, backgroundPosition: 'center', backgroundSize: 'cover', backgroundRepeat: 'no-repeat' }}>
+                                    <div class="ep">{s.currentChap} / {s.totalChap}</div>
+                                    <div class="like"><i class="bi bi-hand-thumbs-up-fill"></i> {s.likes}</div>
+                                    <div class="view"><i class="bi bi-eye-fill"></i> {s.views}</div>
+                                  </div>
+                                  <div class="product__item__text">
+                                    <h5><a href="#">{s.storyName}</a></h5>
+                                  </div>
+                                </div>
+                              </div>
+                            );
+                          }
+                        })
+                      }
                     </div>
                   </div>
-                </div>
-                <div className="row">
-                  <div className="col-lg-4 col-md-6 col-sm-6">
-                    <StoryItem />
-                  </div>
-                  <div className="col-lg-4 col-md-6 col-sm-6">
-                    <div className="product__item">
-                      <div
-                        className="product__item__pic set-bg"
-                        data-setbg="img/popular/popular-1.jpg"
-                      >
-                        <div className="wrapper_img">
-                          <img src={img1} alt="" />
-                        </div>
-                        <div className="ep">18 / 18</div>
-                        <div className="comment">
-                          <i className="fa fa-comments"></i> 11
-                        </div>
-                        <div className="view">
-                          <i className="fa fa-eye"></i> 9141
-                        </div>
-                      </div>
-                      <div className="product__item__text">
-                        <ul>
-                          <li>Active</li>
-                          <li>Movie</li>
-                        </ul>
-                        <h5>
-                          <a href="#">Sen to Chihiro no Kamikakushi</a>
-                        </h5>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col-lg-4 col-md-6 col-sm-6">
-                    <div className="product__item">
-                      <div
-                        className="product__item__pic set-bg"
-                        data-setbg="img/popular/popular-2.jpg"
-                      >
-                        <div className="ep">18 / 18</div>
-                        <div className="comment">
-                          <i className="fa fa-comments"></i> 11
-                        </div>
-                        <div className="view">
-                          <i className="fa fa-eye"></i> 9141
-                        </div>
-                      </div>
-                      <div className="product__item__text">
-                        <ul>
-                          <li>Active</li>
-                          <li>Movie</li>
-                        </ul>
-                        <h5>
-                          <a href="#">Kizumonogatari III: Reiket su-hen</a>
-                        </h5>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col-lg-4 col-md-6 col-sm-6">
-                    <div className="product__item">
-                      <div
-                        className="product__item__pic set-bg"
-                        data-setbg="img/popular/popular-3.jpg"
-                      >
-                        <div className="ep">18 / 18</div>
-                        <div className="comment">
-                          <i className="fa fa-comments"></i> 11
-                        </div>
-                        <div className="view">
-                          <i className="fa fa-eye"></i> 9141
-                        </div>
-                      </div>
-                      <div className="product__item__text">
-                        <ul>
-                          <li>Active</li>
-                          <li>Movie</li>
-                        </ul>
-                        <h5>
-                          <a href="#">Shirogane Tamashii hen Kouhan sen</a>
-                        </h5>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="product__pagination">
-                <a href="#" className="current-page">
-                  1
-                </a>
+                })
+              }
+              <div class="product__pagination">
+                <a href="#" class="current-page">1</a>
                 <a href="#">2</a>
                 <a href="#">3</a>
                 <a href="#">4</a>
                 <a href="#">5</a>
-                <a href="#">
-                  <i className="fa fa-angle-double-right"></i>
-                </a>
+                <a href="#"><i class="fa fa-angle-double-right"></i></a>
               </div>
             </div>
-            <div className="col-lg-4 col-md-6 col-sm-8">
-              <div className="product__sidebar">
-                <div className="product__sidebar__view">
-                  <div className="section-title">
+            <div class="col-lg-4 col-md-6 col-sm-8">
+              <div class="product__sidebar">
+                <div class="product__sidebar__view">
+                  <div class="section-title">
                     <h5>Top Views</h5>
                   </div>
-                  <ul className="filter__controls">
-                    <li className="active" data-filter="*">
-                      Day
-                    </li>
+                  <ul class="filter__controls">
+                    <li class="active" data-filter="*">Day</li>
                     <li data-filter=".week">Week</li>
                     <li data-filter=".month">Month</li>
                     <li data-filter=".years">Years</li>
                   </ul>
-                  <div className="filter__gallery">
-                    <StoryItem2 />
-                    <StoryItem2 />
-                    <StoryItem2 />
+                  <div class="filter__gallery">
+                    <div class="product__sidebar__view__item set-bg mix day years"
+                      data-setbg="img/sidebar/tv-1.jpg">
+                      <div class="ep">18 / ?</div>
+                      <div class="view"><i class="fa fa-eye"></i> 9141</div>
+                      <h5><a href="#">Boruto: Naruto next generations</a></h5>
+                    </div>
+                    <div class="product__sidebar__view__item set-bg mix month week"
+                      data-setbg="img/sidebar/tv-2.jpg">
+                      <div class="ep">18 / ?</div>
+                      <div class="view"><i class="fa fa-eye"></i> 9141</div>
+                      <h5><a href="#">The Seven Deadly Sins: Wrath of the Gods</a></h5>
+                    </div>
+                    <div class="product__sidebar__view__item set-bg mix week years"
+                      data-setbg="img/sidebar/tv-3.jpg">
+                      <div class="ep">18 / ?</div>
+                      <div class="view"><i class="fa fa-eye"></i> 9141</div>
+                      <h5><a href="#">Sword art online alicization war of underworld</a></h5>
+                    </div>
+                    <div class="product__sidebar__view__item set-bg mix years month"
+                      data-setbg="img/sidebar/tv-4.jpg">
+                      <div class="ep">18 / ?</div>
+                      <div class="view"><i class="fa fa-eye"></i> 9141</div>
+                      <h5><a href="#">Fate/stay night: Heaven's Feel I. presage flower</a></h5>
+                    </div>
+                    <div class="product__sidebar__view__item set-bg mix day"
+                      data-setbg="img/sidebar/tv-5.jpg">
+                      <div class="ep">18 / ?</div>
+                      <div class="view"><i class="fa fa-eye"></i> 9141</div>
+                      <h5><a href="#">Fate stay night unlimited blade works</a></h5>
+                    </div>
                   </div>
                 </div>
-                <div className="product__sidebar__comment">
-                  <div className="section-title">
+                <div class="product__sidebar__comment">
+                  <div class="section-title">
                     <h5>New Comment</h5>
                   </div>
-                  <StoryItem3 />
-                  <StoryItem3 />
-                  <div className="product__sidebar__comment__item">
-                    <div className="product__sidebar__comment__item__pic">
-                      <img
-                        src={img1}
-                        alt=""
-                        style={{ width: "100%", height: "60%" }}
-                      />
+                  <div class="product__sidebar__comment__item">
+                    <div class="product__sidebar__comment__item__pic">
+                      <img src="img/sidebar/comment-1.jpg" alt="" />
                     </div>
-                    <div className="product__sidebar__comment__item__text">
+                    <div class="product__sidebar__comment__item__text">
                       <ul>
                         <li>Active</li>
                         <li>Movie</li>
                       </ul>
-                      <h5>
-                        <a href="#">Shirogane Tamashii hen Kouhan sen</a>
-                      </h5>
-                      <span>
-                        <i className="fa fa-eye"></i> 19.141 Viewes
-                      </span>
+                      <h5><a href="#">The Seven Deadly Sins: Wrath of the Gods</a></h5>
+                      <span><i class="fa fa-eye"></i> 19.141 Viewes</span>
+                    </div>
+                  </div>
+                  <div class="product__sidebar__comment__item">
+                    <div class="product__sidebar__comment__item__pic">
+                      <img src="img/sidebar/comment-2.jpg" alt="" />
+                    </div>
+                    <div class="product__sidebar__comment__item__text">
+                      <ul>
+                        <li>Active</li>
+                        <li>Movie</li>
+                      </ul>
+                      <h5><a href="#">Shirogane Tamashii hen Kouhan sen</a></h5>
+                      <span><i class="fa fa-eye"></i> 19.141 Viewes</span>
+                    </div>
+                  </div>
+                  <div class="product__sidebar__comment__item">
+                    <div class="product__sidebar__comment__item__pic">
+                      <img src="img/sidebar/comment-3.jpg" alt="" />
+                    </div>
+                    <div class="product__sidebar__comment__item__text">
+                      <ul>
+                        <li>Active</li>
+                        <li>Movie</li>
+                      </ul>
+                      <h5><a href="#">Kizumonogatari III: Reiket su-hen</a></h5>
+                      <span><i class="fa fa-eye"></i> 19.141 Viewes</span>
+                    </div>
+                  </div>
+                  <div class="product__sidebar__comment__item">
+                    <div class="product__sidebar__comment__item__pic">
+                      <img src="img/sidebar/comment-4.jpg" alt="" />
+                    </div>
+                    <div class="product__sidebar__comment__item__text">
+                      <ul>
+                        <li>Active</li>
+                        <li>Movie</li>
+                      </ul>
+                      <h5><a href="#">Monogatari Series: Second Season</a></h5>
+                      <span><i class="fa fa-eye"></i> 19.141 Viewes</span>
                     </div>
                   </div>
                 </div>
