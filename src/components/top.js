958a9951cf7b225ad710deeb546b4fa8d023e8aa
buitoan3106 <buiductoanfu@gmail.com>
2023-07-03 16:00:39 +0700

    category done

--- src/components/top.js
+++ src/components/top.js
@@ -1,5 +1,12 @@
+import { useEffect, useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 export default function Top() {
+    const [cate, setCate] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:9999/category')
+            .then(response => response.json())
+            .then(json => setCate(json));
+    }, [])
     return (
         <header className="header">
             <div className="container fluid Menu">
@@ -14,13 +21,13 @@ export default function Top() {
                     <div className="col-lg-8 header__menu mobile-menu">
                         <ul className="left">
                             <li><NavLink to={'/'} className={({ isActive }) => isActive ? 'link-active' : 'link'}>Home</NavLink></li>
-                            <li><Link to='/category'>Categories  <span><i className="bi bi-chevron-down" /></span></Link>
+                            <li><Link to='/categories'>Categories  <span><i className="bi bi-chevron-down" /></span></Link>
                                 <ul class="dropdown">
-                                    <li><Link to='/'>Hành động</Link></li>
-                                    <li><Link to='/'>Hài hước</Link></li>
-                                    <li><Link to='/'>Ngôn tình</Link></li>
-                                    <li><Link to='/'>Kinh dị</Link></li>
-                                    <li><Link to='/'>Viễn tưởng</Link></li>
+                                    {
+                                        cate.map(c => (
+                                            <li><Link to={'/category/' + c.categoryName}>{c.categoryName}</Link></li>
+                                        ))
+                                    }
                                 </ul>
                             </li>
                             <li><NavLink to={'/about'} className={({ isActive }) => isActive ? 'link-active' : 'link'}>About Us</NavLink></li>
